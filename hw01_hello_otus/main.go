0c9f744242b0874cd962588eef4ee4dd1806a006
saveljev.roman <saveljev.roman@gmail.com>
2024-08-04 15:38:51 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
+	str := "Hello, OTUS!"
+	rStr := reverse.String(str)
+	fmt.Print(rStr)
 	// Place your code here.
 }
